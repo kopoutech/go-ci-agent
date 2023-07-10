d12a9a57478c826dc28be384981111f5b471e00c
Jyotiplaban Talukdar <jyotiplaban@gmail.com>
2023-07-10 21:48:48 +0530

    added readme and json

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # go-ci-agent
  Go ci agent is a program that parses go ci format to run tasks
+
+Go CI format 
+
+```json
+{"source" : {
+  "url" : "https://github.com/realpython/python-scripts.git",
+  "branch" : "master"
+},
+"program" : {
+  "executable" : "python",
+  "args" : ["29_json_to_yaml.py" , "29_json_test.json"],
+  "dir" : "scripts"
+},
+"capture" : {
+  "stdout" : true,
+  "stdoutFile" : "stdout.io",
+  "stderr" : true,
+  "stderrFile" : "stderr.io",
+  "console" : true
+},
+"config" : {
+  "baseDir" : "/tmp/"
+}}
+```
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"log"
 
 	"github.com/jptalukdar/go-ci-agent/pkg/job"
 	"github.com/jptalukdar/go-ci-agent/pkg/sources/git"
@@ -14,26 +16,56 @@ func main() {
 	// if err != nil {
 	// 	log.Printf("Error in executing %s", err)
 	// }
-	e := job.Execution{
-		Source: &git.GitSource{
-			Url:    "https://github.com/realpython/python-scripts.git",
-			Branch: "master",
-		},
-		Program: job.Program{
-			Executable: "python",
-			Args:       []string{"29_json_to_yaml.py", "29_json_test.json"},
-			Dir:        "scripts", // Relative to git download path
-		},
-		Capture: job.Capture{
-			Stdout:     true,
-			StdoutFile: "stdoutput.io",
-			Stderr:     true,
-			StderrFile: "stderr.io",
-			Console:    true,
-		},
-		Config: job.Config{
-			BaseDir: "tmp",
-		},
+	// e := job.Execution{
+	// 	Source: &git.GitSource{
+	// 		Url:    "https://github.com/realpython/python-scripts.git",
+	// 		Branch: "master",
+	// 	},
+	// 	Program: job.Program{
+	// 		Executable: "python",
+	// 		Args:       []string{"29_json_to_yaml.py", "29_json_test.json"},
+	// 		Dir:        "scripts", // Relative to git download path
+	// 	},
+	// 	Capture: job.Capture{
+	// 		Stdout:     true,
+	// 		StdoutFile: "stdoutput.io",
+	// 		Stderr:     true,
+	// 		StderrFile: "stderr.io",
+	// 		Console:    true,
+	// 	},
+	// 	Config: job.Config{
+	// 		BaseDir: "tmp",
+	// 	},
+	// }
+
+	var e job.Execution
+	e.Source = &git.GitSource{} // This line prevents json unmarshalling issue
+
+	Data := []byte(`{"source" : {
+		"url" : "https://github.com/realpython/python-scripts.git",
+		"branch" : "master"
+	},
+	"program" : {
+		"executable" : "python",
+		"args" : ["29_json_to_yaml.py" , "29_json_test.json"],
+		"dir" : "scripts"
+	},
+	"capture" : {
+		"stdout" : true,
+		"stdoutFile" : "stdout.io",
+		"stderr" : true,
+		"stderrFile" : "stderr.io",
+		"console" : true
+	},
+	"config" : {
+		"baseDir" : "/tmp/"
+	}}`)
+
+	// decoding country1 struct
+	// from json format
+	err := json.Unmarshal(Data, &e)
+	if err != nil {
+		log.Fatal(err)
 	}
 	e.Run(ctx)
 }
